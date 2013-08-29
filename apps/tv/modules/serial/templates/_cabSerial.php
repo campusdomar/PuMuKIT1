569b2d9593db9a175e92b72b2fedc05da794d361
rubenrua <rubenrua@eaf164fd-8abb-441f-bb6f-2618a7784eac>
2013-08-29 09:10:45 +0000

    Borro info de signado
    git-svn-id: https://172.20.209.163/svn/pumukit/branches/1.8@7761 eaf164fd-8abb-441f-bb6f-2618a7784eac

--- apps/tv/modules/serial/templates/_cabSerial.php
+++ apps/tv/modules/serial/templates/_cabSerial.php
@@ -1,11 +1,7 @@
 <div class="<?php echo $serial->getBroadcastMax()->getBroadcastType()->getName()?>"></div>
 
-<?php if($serial->hasSubtitles()): ?>
-  <img style="float:right" src="/images/tv/iconos/simbolo_manos_40.png" alter="signado" />
-<?php endif ?>
-
 <div class="cab_serial">
-<!--  <h1 class="title">
+  <!--<h1 class="title">
     <?php echo $serial->getTitle() ?>
   </h1>-->
 
