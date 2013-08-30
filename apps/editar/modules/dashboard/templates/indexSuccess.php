738386aac761ac5d8dd2f551b24e9bd5a2b928b7
andres <andres@eaf164fd-8abb-441f-bb6f-2618a7784eac>
2013-08-30 13:00:38 +0000

    #3830 Elimino el enlace incorrecto ("discute") de las burbujas del dashboard y traduzco la fecha. Igual que el módulo timeline.
    git-svn-id: https://172.20.209.163/svn/pumukit/branches/1.8@7800 eaf164fd-8abb-441f-bb6f-2618a7784eac

--- apps/editar/modules/dashboard/templates/indexSuccess.php
+++ apps/editar/modules/dashboard/templates/indexSuccess.php
@@ -15,9 +15,9 @@
   <?php endforeach ?>
 </div>
 
+<script type="text/javascript">
 
 
-<?php echo javascript_tag("
 Event.observe(window, 'load', function() { 
   var eventSource = new Timeline.DefaultEventSource();
   var bandInfos = [
@@ -42,9 +42,30 @@ Event.observe(window, 'load', function() {
   tl = Timeline.create(document.getElementById('my-timeline'), bandInfos);
   Timeline.loadXML('/editar.php/dashboard/serial_timeline.xml', function(xml, url) { eventSource.loadXML(xml, url); });
 
-})
-");
-?>
+});
 
+<?php  // Sobrecargo la función que pinta la burbuja y sólo pinto el título y la fecha traducida al español ?>
+var oldFillInfoBubble = Timeline.DefaultEventSource.Event.prototype.fillInfoBubble;
+var weekday = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
+var monthname = new Array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+Timeline.DefaultEventSource.Event.prototype.fillInfoBubble = function(elmt, theme, labeller) {
+    var eventObject = this;
+    
+    // oldFillInfoBubble.call(this, elmt, theme, labeller); // antigua función para crear el html de la burbuja
+    var start = new Date(eventObject._start);
+    var start_es = weekday[start.getDay()] + " " + start.getDate() + " de " + monthname[start.getMonth()] + " de " + start.getFullYear() + " " + start.toLocaleTimeString();
+    var div_title = document.createElement("div");
+    div_title.className = "timeline-event-bubble-title";
+    var a_title = document.createElement("a");
+    a_title.setAttribute('href', eventObject._link);
+    a_title.innerHTML = eventObject._text;
+    div_title.appendChild(a_title);
+    var div_time = document.createElement("div");
+    div_time.className = "timeline-event-bubble-time";
+    div_time.innerText = start_es;
+    elmt.appendChild(div_title);
+    elmt.appendChild(div_time);
+}
+</script>
 <br />
 <br />
