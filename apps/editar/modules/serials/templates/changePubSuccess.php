2102a9b8f82ee40bb9014cea57b26e1270604b68
nacho <nacho@eaf164fd-8abb-441f-bb6f-2618a7784eac>
2014-04-23 16:27:19 +0000

    Permite que se marque webtv aunque el video no sea publico. reportado en #4794
    git-svn-id: https://172.20.209.163/svn/pumukit/branches/1.8@8854 eaf164fd-8abb-441f-bb6f-2618a7784eac

--- apps/editar/modules/serials/templates/changePubSuccess.php
+++ apps/editar/modules/serials/templates/changePubSuccess.php
@@ -60,15 +60,13 @@ if(this.value.length != 0){
             <?php foreach($pubs as $p): ?>
 	      <td style="text-align:center; background: transparent">
                 <input type="checkbox"  
-<?php if($p->getEnable() == 0):?>
-disabled="disabled"
-<?php else: ?>
- onchange="
-var val = this.checked;
-$('table_mms_change_pub').select('input.pub_channel_change_pub_<?php echo $p->getId()?>').each(function(s){s.checked = val});
-                 "
-<?php endif ?>
-              />
+                  <?php if($p->getEnable() == 0):?>
+                      disabled="disabled"
+                  <?php else: ?>
+                      onchange=" var val = this.checked;
+                     $('table_mms_change_pub').select('input.pub_channel_change_pub_<?php echo $p->getId()?>').each(function(s){s.checked = val});"
+                  <?php endif ?>
+                />
               </td>
             <?php endforeach ?>
           </tr>
@@ -103,7 +101,7 @@ $('table_mms_change_pub').select('input.pub_channel_change_pub_<?php echo $p->ge
                            name="data[<?php echo $mm->getId()?>][pub_channels][<?php echo $p->getId()?>]" 
                            class="pub_channel_input_checkbox pub_channel_change_pub_<?php echo $p->getId()?>"
                            <?php echo ($estado !== 0)?'checked="checked"':""?>
-                           <?php echo (($mm->getStatusId() == 1)||($estado === 2)||($estado === 3))?'disabled="disabled"':''?>
+                           <?php echo (($estado === 2)||($estado === 3))?'disabled="disabled"':''?>
                     />
                   </td>
                 <?php endif ?>
