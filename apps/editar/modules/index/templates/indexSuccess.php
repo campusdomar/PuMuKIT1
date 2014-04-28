d54185b84e1a419b0fe8b5efe97b6e95c07eb1f3
rubenrua <rubenrua@eaf164fd-8abb-441f-bb6f-2618a7784eac>
2014-04-28 16:28:45 +0000

    i18n modules index
    git-svn-id: https://172.20.209.163/svn/pumukit/branches/1.8@8896 eaf164fd-8abb-441f-bb6f-2618a7784eac

--- apps/editar/modules/index/templates/indexSuccess.php
+++ apps/editar/modules/index/templates/indexSuccess.php
@@ -11,14 +11,14 @@
   				 'script' => 'true',
   				 )) ?>
     
-    <?php echo label_for('login', 'Login:', 'class="required" ') ?>
+    <?php echo label_for('login', __('Login:'), 'class="required" ') ?>
     <div id="input_login">
       <?php echo input_tag('login')?>
     </div>
     
     <br />
     
-    <?php echo label_for('passwd', 'Passwd:', 'class="required" ') ?>
+    <?php echo label_for('passwd', __('Passwd:'), 'class="required" ') ?>
     <div id="input_login">
       <?php echo input_password_tag('passwd')?>
     </div>
@@ -31,11 +31,11 @@
     
     <div id="ok_login">
       <?php if($sf_request->getParameter('error') == 2):?>
-        <span id="noSession" class="error" style="float:left; ">Sesi&oacute;n expirada&nbsp;</span>
+        <span id="noSession" class="error" style="float:left; "><?php echo __('Sesi&oacute;n expirada')?>&nbsp;</span>
       <?php endif ?>
-      <span id="noEstandar" class="error" style="display:none; float:left; ">No compatible con IE</span>
-      <span id="noUser" class="error" style="display:none; float:left; ">ERROR de LOGIN</span>
-      <?php echo submit_tag('OK', 'class=ok')?>
+      <span id="noEstandar" class="error" style="display:none; float:left; "><?php echo __('No compatible con IE')?></span>
+      <span id="noUser" class="error" style="display:none; float:left; "><?php echo __('ERROR de LOGIN')?></span>
+      <?php echo submit_tag(__('OK'), 'class=ok')?>
     </div>
   
     <?php if(isset($url)) echo input_hidden_tag('url', substr(strtr($url, ' ', '/'), 1)) ?>  
